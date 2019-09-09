547458d611ac62ea9d8bbcb8bea8a3966c1a0b09
Andreas Kloeckner <inform@tiker.net>
2019-09-09 13:08:57 -0500

    Add merge stmt script

---
+++ contrib/merge-stmt.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# A script to merge the ongoing stream of statements into one main database while keeping
+# backups.
+
+set -e
+
+NEW_STMT="$1"
+
+if test "$NEW_STMT" = ""; then
+  echo "usage: $0 statement.pdf"
+  exit 1
+fi
+
+STMT_BASE="$(basename $NEW_STMT)"
+STMT_BASE="${STMT_BASE%.pdf}"
+STMT_BASE="${STMT_BASE%.pdf}"
+
+if ls *-journal 1> /dev/null 2>&1; then
+  echo "Database has live rollback journal, aborting."
+  exit 1
+fi
+
+TIMESTAMP="$(date +%Y-%m-%d-%H%M-%S)"
+
+if ! which read-uiuc-fin-statement 1> /dev/null 2>&1; then
+  echo "statement reader binary not found"
+  exit 1
+fi
+
+cp "statements.sqlite3" "backup/statements-$TIMESTAMP-before-$STMT_BASE.sqlite3"
+
+read-uiuc-fin-statement "$NEW_STMT"
